84c98df5846672b2d77a31571a5e87b2cd678476
Mergery-GameDev <mergerywanjirumukoma@gmail.com>
2025-11-12 14:51:17 +0300

    Add: Dummy Script
    Added a script to showcase collaboration

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // THIS IS AN EXAMPPLE OF A CHANGE 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
